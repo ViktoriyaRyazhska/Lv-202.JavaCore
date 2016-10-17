22b9f313fecd4f61d1fe196abd3e5aa540119724
OlenaGalay <olenagalay.lviv@gmail.com>
2016-10-17 16:55:56 +0300

    Delete super.

--- HW_5/Car.java
+++ HW_5/Car.java
@@ -1,18 +1,19 @@
 
 public class Car {
-String type;
-int year;
-double capacity;
-public Car(String type, int year, double capacity) {
-	super();
-	this.type = type;
-	this.year = year;
-	this.capacity = capacity;
-}
-@Override
-public String toString() {
-	return "Car [type=" + type + ", year=" + year + ", capacity=" + capacity + "]";
-}
+	String type;
+	int year;
+	double capacity;
 
+	public Car(String type, int year, double capacity) {
+
+		this.type = type;
+		this.year = year;
+		this.capacity = capacity;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [type=" + type + ", year=" + year + ", capacity=" + capacity + "]";
+	}
 
 }
