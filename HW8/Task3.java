ba71a46acb5cc3291987e13cc202e342c9d2989f
OlenaGalay <olenagalay.lviv@gmail.com>
2016-10-22 23:22:16 +0300

    Improve the pattern

--- HW8/Task3.java
+++ HW8/Task3.java
@@ -9,12 +9,12 @@ public class Task3 {
 
 		System.out.println("Enter US currency");
 		text = sc.next();
-		String pattern = "[$][0-9]+[.][0-9]{2}";
+		String pattern = "[$][0-9]+([.][0-9]{2})?";
 
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher(text);
 		while (m.find()) {
-			System.out.print(text.substring(m.start(), m.end()) + "\n");
+			System.out.print(text.substring(m.start(), m.end()));
 		}
 
 	}
